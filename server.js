7a45b5000d615bd67d68ad1ea4456e86978d6f6e
Umer <umer@tutamail.com>
2016-10-31 08:31:28 +0000

    Changed

--- server.js
+++ server.js
@@ -84,5 +84,5 @@
 
     
     // listen (start app with node server.js) ======================================
-    app.listen(8080);
+    app.listen(process.env.PORT || 3000);
     console.log("App listening on port 8080");
